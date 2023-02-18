c586aee21dab4ad6e1620bfdae296d3d882168d4
Shushant <shushantdubey27@gmail.com>
2023-02-19 00:19:18 +0530

    Mongo Connection

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express';
 import { config } from 'dotenv';
+import mongoose  from 'mongoose';
 
 config();
 
 const app = express();
+
+mongoose.connect(process.env.DB_URI as string);
+
 
 app.all('/', (req: Request, res: Response) => {
   res.send('Hello World!');
